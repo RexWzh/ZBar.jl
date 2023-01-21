fcbda16dc230164bc2a24748f10ee3bfa0eb15b5
Documenter.jl <documenter@juliadocs.github.io>
2023-01-21 03:29:20 +0000

    build based on 74d35d0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = zbar","category":"page"},{"location":"#zbar","page":"Home","title":"zbar","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for zbar.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [zbar]","category":"page"}]
+}
