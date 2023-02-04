ac1dc3ebafaf93767fb5ce4bdcee3c32afaf0b7f
rex <1073853456@qq.com>
2023-02-04 11:04:37 +0800

    update test

--- README.md
+++ README.md
@@ -1,6 +1,45 @@
 # zbar
 
+Wrapper of the C library [zbar](http://zbar.sourceforge.net/) for Julia.
+
 [![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://RexWzh.github.io/zbar.jl/stable)
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://RexWzh.github.io/zbar.jl/dev)
 [![Build Status](https://github.com/RexWzh/zbar.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/RexWzh/zbar.jl/actions/workflows/CI.yml?query=branch%3Amain)
 [![Coverage](https://codecov.io/gh/RexWzh/zbar.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/RexWzh/zbar.jl)
+
+## Products
+The build tarball of zbar_jll contains two products:
+  - `libzbar.so`
+  - `zbarimg`
+
+This repository wraps the content of `libzbar.so` via [Clang.jl](https://github.com/JuliaInterop/Clang.jl), and provides a Julia interface for the binary product `zbarimg`.
+
+# Usage
+
+## Installation
+
+```julia
+using Pkg
+Pkg.add("zbar")
+```
+
+## Examples
+
+Pass the arguments to `zbarimg`:
+```julia
+using zbar
+res = execute(`$(zbarimg()) --help`)
+println(res.stdout, '\n', res.code, '\n', res.stderr)
+```
+
+Or use the wrappers `decodeimg` and `decodesingle`
+```julia
+using zbar, QRCoders, Test
+# decode a single QR-Code
+exportqrcode("hello world!", "hello.png")
+@test decodesingle("hello.png") == "hello world!" # "hello world!"
+
+# decode all QR-Codes
+exportqrcode(["hello", "world"], "hello_world.gif")
+@test decodeimg("hello_world.gif") == ["hello", "world"] # ["hello", "world"]
+```
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,58 @@
 using zbar
 using Test
+using QRCoders
 
-@testset "zbar.jl" begin
-    
+testpath = "testpath"
+
+
+@testset "zbar.jl -- general test" begin
+    ### `zbarimg` ###
+    println(execute(`$(zbarimg()) --help`).stdout)
+    @test true
+
+    ### `decodeimg` ###
+    # normal test for `.png`
+    exportqrcode("hello world", "$testpath/hello.png")
+    @test decodeimg("$testpath/hello.png")[1] == "hello world"
+
+    # QRCode(s) with empty text
+    exportqrcode("", "$testpath/empty.jpeg")
+    @test decodeimg("$testpath/empty.jpeg")[1] == ""
+    exportqrcode(["", ""], "$testpath/empty.gif")
+    @test decodeimg("$testpath/empty.gif") == ["", ""]
+
+    # QRCode with `\n`
+    exportqrcode("hello\nworld", "$testpath/hellonewline.jpg")
+    @test decodeimg("$testpath/hellonewline.jpg")[1] == "hello\nworld"
+
+    # lots of QRCodes
+    exportqrcode(fill("hello", 12), "$testpath/hello12.gif")
+    @test decodeimg("$testpath/hello12.gif") == fill("hello", 12)
+
+    # rotate
+
+    # flip
+
+    # blank image
+
+    # QRCode with misleading information
+
+
+    ### `decodesingle` ###
+    @test decodesingle("$testpath/hello.png") == "hello world"
+    @test decodesingle("$testpath/empty.jpeg") == ""
+    @test decodesingle("$testpath/hellonewline.jpg") == "hello\nworld"
+    @test_throws ErrorException decodesingle("$testpath/empty.gif")
+    @test_throws ErrorException decodesingle("$testpath/hello12.gif")
+
+end
+
+@testset "Rules compare to QRCoders -- TODO" begin
+    # Encoding mode vs QRCoders
+
+    # mask from 0 to 7
+
+    # error correction levels
+
+    # version from 1 to 40
 end
